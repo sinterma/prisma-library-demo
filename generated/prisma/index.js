e3d8ebbfe06d3687558b56ed99e149646a071f7f
Sosha Fedotovskaia <sinterma@gmail.com>
2025-04-24 12:05:31 +0200

    new message

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -170,11 +170,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:12345@localhost:5432/prismaLibraryDemo?schema=public"
+        "value": null
       }
     }
   },
